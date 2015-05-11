a6ae0d62b6b265eaa126f0c24125081430659ffd
Jacob Holtom <webmasterlegobrains@gmail.com>
2015-05-11 15:06:32 -0400

    slow improvement and expansion

--- lpc/dip28/adc-pass/main.c
+++ lpc/dip28/adc-pass/main.c
@@ -3,70 +3,69 @@
 #include "lpc111x.h"
 #include <stdint.h>
 #include <stdlib.h>
+
+#define adelay 500 //Half a second delay for the audio
+#define delaysamples (adelay * 44.1) //Its a delayed sample
+#define decay 0.5
 void delay(int dly)
 {
-    while (dly--);
+	while (dly--);
 }
 void ConfigPins()
 {
-    SYSAHBCLKCTRL |= BIT6 + BIT13 + BIT16; // Turn on clock for GPIO, ADC and IOCON 
-    // Output pin cofiguration
-    // Port 1 Bit 1 -> generic io
-    IOCON_R_PIO1_1 |= BIT0;
-    // Port 1 Bit 2 -> generic io
-    IOCON_R_PIO1_2 |= BIT0;
-    // Port 1 Bit 3 -> generic io
-    IOCON_SWDIO_PIO1_3 |= BIT0;
-    // Port 1 bit 4 behaves as a generic IO port by default
-    // Port 1 bit 5 behaves as a generic IO port by default
-    // Port 1 bit 6 behaves as a generic IO port by default
-    // Port 1 bit 7 behaves as a generic IO port by default
-    // Port 1 bit 8 behaves as a generic IO port by default
-    // Configure output addressing
-    GPIO1DIR |= (BIT1+BIT2+BIT3+BIT4+BIT5+BIT6+BIT7+BIT8);
-    GPIO1DATA=0;
+	SYSAHBCLKCTRL |= BIT6 + BIT13 + BIT16; // Turn on clock for GPIO, ADC and IOCON 
+	// Output pin cofiguration
+	// Port 1 Bit 1 -> generic io
+	IOCON_R_PIO1_1 |= BIT0;
+	// Port 1 Bit 2 -> generic io
+	IOCON_R_PIO1_2 |= BIT0;
+	// Port 1 Bit 3 -> generic io
+	IOCON_SWDIO_PIO1_3 |= BIT0;
+	// Port 1 bit 4 behaves as a generic IO port by default
+	// Port 1 bit 5 behaves as a generic IO port by default
+	// Port 1 bit 6 behaves as a generic IO port by default
+	// Port 1 bit 7 behaves as a generic IO port by default
+	// Port 1 bit 8 behaves as a generic IO port by default
+	// Configure output addressing
+	GPIO1DIR |= (BIT1+BIT2+BIT3+BIT4+BIT5+BIT6+BIT7+BIT8);
+	GPIO1DATA=0;
 
-    // Get the ADC started
-    // select analog mode for PIO1_0
-    IOCON_R_PIO1_0 = 2;
-    // Power up the ADC
-    PDRUNCFG &= ~BIT4; 
-    // set ADC clock rate and select channel 1
-    AD0CR=(11<<8)+BIT1;
+	// Get the ADC started
+	// select analog mode for PIO1_0
+	IOCON_R_PIO1_0 = 2;
+	// Power up the ADC
+	PDRUNCFG &= ~BIT4; 
+	// set ADC clock rate and select channel 1
+	AD0CR=(11<<8)+BIT1;
 
 }
 int ReadADC()
 {
-    // Start a conversion
-    AD0CR |=BIT1+BIT24;
-    // Wait for conversion to complete
-    while((AD0GDR&BIT31)==0);
-    // return the result
-    return ((AD0GDR>>8));  //Shift from 6 for all 10bits [6:15]
+	// Start a conversion
+	AD0CR |=BIT1+BIT24;
+	// Wait for conversion to complete
+	while((AD0GDR&BIT31)==0);
+	// return the result
+	return ((AD0GDR>>8));  //Shift from 6 for all 10bits [6:15]
 }
 void Output(uint8_t data)
 {
-    GPIO1DATA = data;
-/*Manual writing works fine now
-    GPIO1DATA |= BIT1;
-    GPIO1DATA |= BIT2;
-    GPIO1DATA |= BIT3;
-    GPIO1DATA |= BIT4;
-    GPIO1DATA |= BIT5;
-    GPIO1DATA |= BIT6;
-    GPIO1DATA |= BIT7;
-    GPIO1DATA |= BIT8;
-    GPIO1DATA &= ~BIT4;
-    GPIO1DATA &= ~BIT6;
-    GPIO1DATA &= ~BIT8; */
+	GPIO1DATA = data;
 }
 int main()
 {	
-    ConfigPins();
+	ConfigPins();
 
-while(1) 
-    {
-        Output(ReadADC());
-	delay(23);
-    }    
+	while(1) 
+	{
+		data = ReadADC();
+		int bufloc = 0;
+		uint8_t buffer[50000];	
+		for (int i = 0; i < 50000 - delaysamples; i++)
+		{
+
+		} 
+		Output(out);
+		delay(23);
+	}    
 }
